ef49811d2a0176736bb7cb1a0b19d0b6aed9652c
Pixdigit <max12354678910@gmail.com>
2018-03-01 22:38:08 +0100

    Decided to change project to do something else. Initial Commit?

--- internal/physicsEngine/physicsEngine.go
+++
@@ -1,9 +0,0 @@
-package physicsEngine
-
-import "github.com/ByteArena/box2d"
-
-func CreateWorld(xGravity,yGravity float64) (box2d.B2World){
-    gravity := box2d.MakeB2Vec2(xGravity, yGravity)
-    world := box2d.MakeB2World(gravity)
-    return world
-}
