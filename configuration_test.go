2ed7bcdc9e2138f359f9428560c2d316a2a58b26
Pixdigit <max12354678910@gmail.com>
2018-03-04 23:30:17 +0100

    added configuration interface

--- configuration_test.go
+++ configuration_test.go
@@ -10,7 +10,7 @@ func TestSerialization(t *testing.T) {
     testVars := []interface{}{
         true, false, "true",
         3, 456, -234, -0,
-        3.1415, -2.1645, -0.0, 
+        3.1415, -2.1645, -0.0,
         "4", "str:test", ":", "",
     }
 
@@ -23,4 +23,21 @@ func TestSerialization(t *testing.T) {
             }
         }
     }
+}
+
+func TestConfigurationSystem(t *testing.T) {
+    confs := map[string]interface{}{
+        "test1": 0,
+        "test2": 3.1415,
+        "": true,
+        "äüß \n ?=)(/&%$§!)": "OKAYdokay",
+    }
+    for k, v := range confs {
+        err := AddConf(k, v)
+        if err != nil {t.Error("error while adding configuration", err)}
+        confValue, err := GetConf(k)
+        if err != nil {t.Error("could not read back configuration", err)}
+        test(confValue == v, "configuration is not equal to set value", t)
+    }
+
 }
--- env.go
+++ env.go
@@ -15,7 +15,9 @@ var displayIndex int //TODO: Dynamically update when window moved
 var conf map[string]string = map[string]string{}
 
 func init() {
+    //TODO: Actually use configuration
     conf["updateOnRefresh"] = "bool:true"
+    conf["fullscreen"] = "bool:true"
 }
 
 func serialize(variable interface{}) (string, error) {
@@ -43,8 +45,12 @@ func serialize(variable interface{}) (string, error) {
     return result, err
 }
 
+func typeOfSerialized(s string) string {
+    return s[:strings.Index(s, ":")]
+}
+
 func deserialize(raw string) (result interface{}, err error) {
-    varType := raw[:strings.Index(raw, ":")]
+    varType := typeOfSerialized(raw)
     varValue := raw[strings.Index(raw, ":") + 1:]
 
     switch varType {
@@ -68,4 +74,29 @@ func deserialize(raw string) (result interface{}, err error) {
         err = errors.New("unknown var type in deserialisation: " + varType)
     }
     return
+}
+
+func GetConf(confName string) (interface{}, error) {
+    config, ok := conf[confName]
+    if !ok {return nil, errors.New("configuration " + confName + " does not exist")}
+    value, err := deserialize(config)
+    if err != nil {return nil, errors.New("invalid configuration value:")}
+    return value, nil
+}
+
+func SetConf(confName string, confValue interface{}) error {
+    config, ok := conf[confName]
+    if !ok {return errors.New("configuration " + confName + " does not exist")}
+    newConfig, err := serialize(confValue)
+    if err != nil {return errors.Wrap(err, "could not change configuration:")}
+    if typeOfSerialized(config) != typeOfSerialized(newConfig) {return errors.New("configuration must have same type")}
+    conf[confName] = newConfig
+    return nil
+}
+
+func AddConf(confName string, initConfValue interface{}) error {
+    newConfig, err := serialize(initConfValue)
+    if err != nil {return errors.Wrap(err, "could not add configuration:")}
+    conf[confName] = newConfig
+    return nil
 }
