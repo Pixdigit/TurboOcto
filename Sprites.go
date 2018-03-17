579144b64c9290c67cd6dd2f7cb4970e82c01ef5
Pixdigit <max12354678910@gmail.com>
2018-03-17 03:47:53 +0100

    improved Sprite Loading

--- Sprites.go
+++ Sprites.go
@@ -7,42 +7,59 @@ import (
 )
 
 type Sprite struct {
-    frames  []*sdl.Texture
+    frames  []sdl.Texture
     delays  []int32
     XCenter, YCenter int32
-    Width, Height int32
-    animationDelay int32
 }
 
 func NewSprite() (Sprite, error) {
     return Sprite{}, nil
 }
+func LoadAnimatedSpriteFromTextures(textures []sdl.Texture, delays []int32) (Sprite, error) {
+    if (len(textures) != len(delays)) {return Sprite{}, errors.New("argument lengths must be equal \"textures " + string(len(textures)) + "  delays " + string(len(delays)))}
 
-func LoadAnimatedSprite(fileNames []string, delays []int32, rects []sdl.Rect) (Sprite, error) {
-    if (len(fileNames) != len(delays)) || (len(delays) != len(rects)) {return Sprite{}, errors.New("argument lengths must be equal \"files " + string(len(fileNames)) + "  delays " + string(len(delays)) + "  rects " + string(len(rects)))}
-
-    sprite := Sprite{}
-
-    for i, fileName := range fileNames {
-        ok, err := pathExists("./assets/sprites/" + fileName);    if err != nil {return Sprite{}, errors.Wrap(err, "could not check if path exists \"./assets/sprites/" + fileName + "\"")}
-        if !ok {return Sprite{}, errors.New("sprite file does not exist \"./assets/sprites/" + fileName + "\"")}
-        surface, err := img.Load("./assets/sprites/" + fileName);    if err != nil {return Sprite{}, errors.Wrap(err, "could not load sprite file \"./assets/sprites/" + fileName)}
-
-        tmpSurface, err := sdl.CreateRGBSurface(0, rects[i].W, rects[i].H, 32, rmask, gmask, bmask, amask);    if err != nil {return Sprite{}, errors.Wrap(err, "could not create tmpSurface for transfer")}
-        //Centered to tmpSurface
-        srcRect := sdl.Rect{(rects[i].W - surface.W) / 2, (rects[i].H - surface.H) / 2, rects[i].W, rects[i].H}
-        surface.Blit(&srcRect, tmpSurface, nil)
-        texture, err := renderer.CreateTextureFromSurface(tmpSurface);    if err != nil {return Sprite{}, errors.Wrap(err, "could not transfer surface to texture")}
-
-        sprite.frames = append(sprite.frames, texture)
-        sprite.delays = append(sprite.delays, delays[i])
-    }
-
-    return sprite, nil
+    return Sprite{
+        frames: textures,
+        delays: delays,
+    }, nil
 }
+func LoadAnimatedSpriteFromFiles(fileNames []string, delays []int32) (Sprite, error) {
+    var textures []sdl.Texture
+    for _, fileName := range fileNames {
+        texture, err := img.LoadTexture(renderer, "./assets/sprites/" + fileName);    if err != nil {return Sprite{}, errors.Wrap(err, "could not load sprite file \"./assets/sprites/" + fileName)}
+        textures = append(textures, *texture)
+    }
+    return LoadAnimatedSpriteFromTextures(textures, delays)
+}
+func LoadSpriteFromFile(filename string) (Sprite, error) {
+    return LoadAnimatedSpriteFromFiles([]string{filename}, []int32{0})
+}
+//IDEA: Support GIF as textures and delays source
+func LoadAnimatedSpriteFromFile(filename string, rects []sdl.Rect, delays []int32) (Sprite, error) {
+    surface, err := img.Load(filename);    if err != nil {return Sprite{}, errors.Wrap(err, "could not load sprite image")}
+    if len(rects) == 0 {
+        //D == Amount of
+        DSprites := surface.W / surface.H
+        for i := int32(0); i < DSprites; i++ {
+            rects = append(rects, sdl.Rect{i * surface.H, 0, surface.H, surface.H})
+        }
+    }
+    var textures []sdl.Texture
+    xOffset := int32(0)
+    for _, rect := range rects {
+        if rect.W == 0 || rect.H == 0 {
+            rect = sdl.Rect{0, 0, surface.H, surface.H}
+        }
+        tmpSurface, err := sdl.CreateRGBSurface(0, rect.W, rect.H, 32, rmask, gmask, bmask, amask);    if err != nil {return Sprite{}, errors.Wrap(err, "could not create tmpSurface for transfer")}
+        rect.X += xOffset
+        xOffset += rect.W
+        surface.Blit(&rect, tmpSurface, nil)
+        texture, err := renderer.CreateTextureFromSurface(tmpSurface);    if err != nil {return Sprite{}, errors.Wrap(err, "could not transfer surface to texture")}
+        textures = append(textures, *texture)
 
-/*
-spriteSheet, err := img.Load("./assets/sprites/" + fileName);    if err != nil {return nil, errors.Wrap(err, "could not load sprite sheet \"./assets/sprites/" + fileName + "\"")}
-spriteSheet.Lock()
-animationLength := spriteSheet.W / spriteSheet.H
-*/
+    }
+    return Sprite{
+        frames: textures,
+        delays: delays,
+        }, nil
+}
