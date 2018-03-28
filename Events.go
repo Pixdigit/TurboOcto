61b3bc72494b908ba3dedd3abc239c65379326c2
Pixdigit <max12354678910@gmail.com>
2018-03-28 09:15:29 +0200

    reworked event handling because of weird typing due to go not supporting unions

--- Events.go
+++ Events.go
@@ -1,20 +1,81 @@
 package TurboOcto
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+    "github.com/veandco/go-sdl2/sdl"
+)
 
-var Events []sdl.Event
+type MouseState struct {
+    X, Y int32
+    XRel, YRel int32
+    Scrolled int32
+    ScrollRelative int32
+    ButtonsHeld []bool
+    ButtonsClicked []bool
+    ButtonsReleased []bool
+}
 
-func UpdateEvents() []sdl.Event {
-    Events = []sdl.Event{}
-    for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-/*        switch e := event.(type) {
-        case sdl.MouseButtonEvent:
-            x, y := scalePoint(e.X, e.Y)
-            e.X, e.Y = x, y
-        }*/
-        Events = append(Events, event)
+var Mouse MouseState
+
+func init() {
+    Mouse = MouseState {
+        0, 0, 0, 0, 0, 0,
+        make([]bool, 3),
+        make([]bool, 3),
+        make([]bool, 3),
     }
-    return Events
+}
+
+func UpdateEvents() {
+    //Reset frame dependend variables
+    Mouse.XRel, Mouse.YRel = 0, 0
+    Mouse.ScrollRelative = 0
+    for i := range Mouse.ButtonsClicked { Mouse.ButtonsClicked[i] = false }
+    for i := range Mouse.ButtonsReleased { Mouse.ButtonsReleased[i] = false }
+
+    for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+        switch e := event.(type) {
+        case *sdl.MouseButtonEvent:
+            x, y := scalePoint(e.X, e.Y)
+            Mouse.X, Mouse.Y = x, y
+            if e.Type == sdl.MOUSEBUTTONDOWN {
+                switch e.Button {
+                case sdl.BUTTON_LEFT:
+                    setMouseButtonState(1, true)
+                case sdl.BUTTON_MIDDLE:
+                    setMouseButtonState(2, true)
+                case sdl.BUTTON_RIGHT:
+                    setMouseButtonState(3, true)
+                case sdl.BUTTON_X1:
+                    setMouseButtonState(4, true)
+                case sdl.BUTTON_X2:
+                    setMouseButtonState(5, true)
+                }
+            } else if e.Type == sdl.MOUSEBUTTONUP {
+                switch e.Button {
+                case sdl.BUTTON_LEFT:
+                    setMouseButtonState(1, false)
+                case sdl.BUTTON_MIDDLE:
+                    setMouseButtonState(2, false)
+                case sdl.BUTTON_RIGHT:
+                    setMouseButtonState(3, false)
+                case sdl.BUTTON_X1:
+                    setMouseButtonState(4, false)
+                case sdl.BUTTON_X2:
+                    setMouseButtonState(5, false)
+                }
+            }
+        case *sdl.MouseMotionEvent:
+            //TODO: Change relative motion too
+            x, y := scalePoint(e.X, e.Y)
+            Mouse.X, Mouse.Y = x, y
+            Mouse.XRel, Mouse.YRel = e.XRel, e.YRel
+        case *sdl.MouseWheelEvent:
+            x, y := scalePoint(e.X, e.Y)
+            Mouse.X, Mouse.Y = x, y
+            Mouse.ScrollRelative = e.X
+            Mouse.Scrolled += e.X
+        }
+    }
 }
 func scalePoint(x, y int32) (int32, int32) {
     if sizer == UNDERFIT_SCALE {
@@ -24,4 +85,23 @@ func scalePoint(x, y int32) (int32, int32) {
         y = int32(float64(yOffset) + yScale * float64(y))
     }
     return x, y
+}
+func setMouseButtonState(buttonIndex int32, isDown bool) {
+    for len(Mouse.ButtonsClicked) - 1 < int(buttonIndex) {
+        Mouse.ButtonsHeld = append(Mouse.ButtonsHeld, false)
+        Mouse.ButtonsClicked = append(Mouse.ButtonsClicked, false)
+        Mouse.ButtonsReleased = append(Mouse.ButtonsReleased, false)
+    }
+
+    if Mouse.ButtonsHeld[buttonIndex] {
+        if !isDown {
+            Mouse.ButtonsReleased[buttonIndex] = true
+            Mouse.ButtonsHeld[buttonIndex] = false
+        }
+    } else {
+        if isDown {
+            Mouse.ButtonsClicked[buttonIndex] = true
+            Mouse.ButtonsHeld[buttonIndex] = true
+        }
+    }
 }
---
+++ events_test.go
@@ -0,0 +1,25 @@
+package TurboOcto
+
+import (
+    "github.com/veandco/go-sdl2/sdl"
+    "github.com/pkg/errors"
+    "testing"
+)
+
+func TestEventScaling(t *testing.T) {
+    e := &sdl.MouseButtonEvent{
+        Type: sdl.MOUSEBUTTONDOWN,
+        Timestamp: 1337,
+        WindowID: 0,
+        Button: sdl.BUTTON_MIDDLE,
+        X: 1337,
+        Y: 1337,
+        State: sdl.PRESSED }
+    filtered, err := sdl.PushEvent(e);    if err != nil {t.Error(errors.Wrap(err, "could not push test event"))}
+    test(!filtered, "test event was not pushed succesfully to the queue", t)
+    Windowed(20, 20)
+    UpdateEvents()
+    test(Mouse.X < 20 && Mouse.X >= 0, "event handler did not scale input", t)
+    test(Mouse.X != 0, "mouse position did not change on mouse event", t)
+
+}
